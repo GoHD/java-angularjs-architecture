6b1b2280c4cc7c2e922f646672f35ddd49c36d4e
Alexandre Zanelatto <alexandre.zanelatto@gmail.com>
2016-07-12 14:27:40 -0300

    Adicionado para redirecionar para o login quando for fazer uma troca de pagina e o token estiver invalido

--- backend/app-rest/src/main/java/com/github/app/rest/LoginRest.java
+++ backend/app-rest/src/main/java/com/github/app/rest/LoginRest.java
@@ -2,6 +2,7 @@ package com.github.app.rest;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -27,6 +28,11 @@ public class LoginRest {
     public Response realizaLogin(LoginDto loginDto) {
         UsuarioLogadoDto usuarioLogadoDto = autenticacaoService.realizaLogin(loginDto);
         return Response.status(Status.OK).entity(usuarioLogadoDto).build() ;
+    }
+    
+    @GET
+    public Response verfificaUsuarioAutenticado() {
+    	return Response.status(Status.OK).build();
     }
 
 }
